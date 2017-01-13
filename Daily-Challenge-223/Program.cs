b014a4a6b5e38e9d54f5e54ea4c984b4c18f9a98
encid <ryancav@gmail.com>
2017-01-13 13:48:26 -0500

    commit

--- Daily-Challenge-223/Program.cs
+++ Daily-Challenge-223/Program.cs
@@ -10,6 +10,25 @@ namespace DailyChallenge223
     {
         static void Main()
         {
+            Console.WriteLine(garland("programmer"));
+            Console.WriteLine(garland("alfalfa"));
+
+            Console.ReadKey();
+        }
+
+        static int garland(string input)
+        {
+            int degree = 0;
+
+            for (int i = 1; i < input.Length; i++) {
+                var start = input.Substring(0, i);
+                var end = input.Substring(input.Length - i, i);
+
+                if (start == end) 
+                    degree = i;                
+            }
+
+            return degree;
         }
     }
 }
