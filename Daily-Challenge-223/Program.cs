a716d30859607b94ee9fe53522ee93f9db74e876
encid <ryancav@gmail.com>
2017-01-13 15:20:44 -0500

    commit

--- Daily-Challenge-223/Program.cs
+++ Daily-Challenge-223/Program.cs
@@ -13,22 +13,35 @@ namespace DailyChallenge223
             Console.WriteLine(garland("programmer"));
             Console.WriteLine(garland("alfalfa"));
 
+            Console.WriteLine(chain("onion", 3));
             Console.ReadKey();
         }
 
-        static int garland(string input)
+        static int garland(string s)
         {
             int degree = 0;
 
-            for (int i = 1; i < input.Length; i++) {
-                var start = input.Substring(0, i);
-                var end = input.Substring(input.Length - i, i);
+            for (int i = 1; i < s.Length; i++) {
+                var start = s.Substring(0, i);
+                var end = s.Substring(s.Length - i, i);
 
                 if (start == end) 
                     degree = i;                
             }
 
             return degree;
+        }
+
+        static string chain(string s, int times)
+        {
+            StringBuilder sb = new StringBuilder(s);
+            int degree = garland(s);
+
+            for (int i = 0; i < times; i++) {
+                sb.Append(s.Substring(degree));
+            }
+
+            return sb.ToString();
         }
     }
 }
